c3b0fc0b4a934c93c6e45fc11631dc0a6a63f07a
Noob Evad <davefrankmiranda@gmail.com>
2023-08-15 12:05:31 -0500

    mostly done with server side, still need to update the Mutations in typeDefs

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,75 @@
+const { User, Book } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate("books");
+    },
+    user: async (parent, { username, _id }) => {
+      return User.findOne({
+        ...(_id ? { _id } : {}),
+        ...(username ? { username } : {}),
+      }).populate("books");
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addBook: async (parent, { body }, context) => {
+      if (context.user) {
+        const { authors, description, bookId, image, link, title } = body;
+
+        const book = await Book.create({
+          authors,
+          description,
+          bookId,
+          image,
+          link,
+          title,
+        });
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { books: book._id } }
+        );
+        return book;
+      }
+      throw AuthenticationError;
+      ("You need to be logged in!");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const book = await Book.findOneAndDelete({
+          bookId: bookId,
+        });
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: book.bookId } },
+          { new: true }
+        );
+        return book;
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String, _id: ID): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBook()
+    
+    addThought(thoughtText: String!): Thought
+    addComment(thoughtId: ID!, commentText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
+`;
+
+module.exports = typeDefs;
