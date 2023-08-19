68adaad9acfdb6f52a17ddb4a4d7b1a33221e881
Noob Evad <davefrankmiranda@gmail.com>
2023-08-19 08:46:05 -0500

    something's wrong with delete but it's time to turn this in and move on

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -10,11 +10,11 @@ const typeDefs = `
   type Book {
     _id: ID
     authors: [String]
-    description: String!
-    bookId: String!
+    description: String
+    bookId: String
     image: String
     link: String
-    title: String!
+    title: String
   }
 
   type Auth {
@@ -41,7 +41,7 @@ const typeDefs = `
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     addBook(input: BookInput!): User
-    removeBook (bookId: String!): User
+    removeBook (bookId: String): User
   }
 `;
 
